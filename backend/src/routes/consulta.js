cb909cc623af482acd5edbc0e38eb0f9780f0553
vict0245 <vf1838829@gmail.com>
2026-07-14 15:18:37 -0500

    Revert "iniciacion y cosas backend"
    This reverts commit bbb01239d2096d96a4b2fcfde374e02e2a59cc94.

--- backend/src/routes/consulta.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const consultaController = require('../controllers/consultaController');
-
-router.post('/consulta', consultaController.responderConsulta);
-
-module.exports = router;
